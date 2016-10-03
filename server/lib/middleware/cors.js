86f3235854b34b75c168273dab9ea912ee850b61
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-10-03 13:56:04 +0700

    start using loopback

--- server/lib/middleware/cors.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(req, res, next) {
-    //CORS!
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', 'accept, content-type');
-    next();
-};
